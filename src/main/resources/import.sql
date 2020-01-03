21593b962e01344b567f2357f19356a51480b2c2
Johan Castano <jach_1997@hotmail.com>
2020-01-02 23:05:23 -0500

    Uso de Cors para compartir recursos en API REST

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO clientes(apellido, create_at, email, nombre)
+VALUES ('Guzman', '2018-01-01', 'A@G.com', 'Andres');
+INSERT INTO clientes(apellido, create_at, email, nombre)
+VALUES ('Guzman1', '2018-01-01', 'A1@G.com', 'Andres1');
+INSERT INTO clientes(apellido, create_at, email, nombre)
+VALUES ('Guzman2', '2018-01-01', 'A2@G.com', 'Andres2');
