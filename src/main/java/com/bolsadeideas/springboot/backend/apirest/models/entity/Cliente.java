8a093d794baa3bae66c02adb153682eec83df888
Johan Castano <jach_1997@hotmail.com>
2020-01-03 15:49:23 -0500

    Customizar mensajes de validación en español

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -15,16 +15,16 @@ public class Cliente implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty
-    @Size(min = 4, max = 12)
+    @NotEmpty(message = "no puede estar vacio")
+    @Size(min = 4, max = 12, message = "el tamaño tiene que estar en 4 y 12")
     @Column(nullable = false)
     private String nombre;
 
-    @NotEmpty
+    @NotEmpty(message = "no puede estar vacio")
     private String apellido;
 
-    @NotEmpty
-    @Email
+    @NotEmpty(message = "No puede estar vacio")
+    @Email(message = "no es una direccion de correo bien formada")
     @Column(nullable = false, unique = true)
     private String email;
 
