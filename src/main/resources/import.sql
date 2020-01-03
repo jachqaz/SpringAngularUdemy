888e0c4b92337ff857920f7ff6fca5d59b69f58f
Johan Castano <jach_1997@hotmail.com>
2020-01-03 08:53:29 -0500

    Escribiendo los métodos update y delete en el Controlador Backend API Rest

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,3 @@
-INSERT INTO clientes(apellido, create_at, email, nombre)
-VALUES ('Guzman', '2018-01-01', 'A@G.com', 'Andres');
-INSERT INTO clientes(apellido, create_at, email, nombre)
-VALUES ('Guzman1', '2018-01-01', 'A1@G.com', 'Andres1');
-INSERT INTO clientes(apellido, create_at, email, nombre)
-VALUES ('Guzman2', '2018-01-01', 'A2@G.com', 'Andres2');
+INSERT INTO clientes(apellido, create_at, email, nombre) VALUES ('Guzman', '2018-01-01', 'A@G.com', 'Andres');
+INSERT INTO clientes(apellido, create_at, email, nombre) VALUES ('Guzman1', '2018-01-01', 'A1@G.com', 'Andres1');
+INSERT INTO clientes(apellido, create_at, email, nombre) VALUES ('Guzman2', '2018-01-01', 'A2@G.com', 'Andres2');
