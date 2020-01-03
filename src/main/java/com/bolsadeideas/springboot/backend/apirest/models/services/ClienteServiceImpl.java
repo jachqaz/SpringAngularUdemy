425498f5599dbffb63ee0f90dd5ccad020d34867
Johan Castano <jach_1997@hotmail.com>
2020-01-02 22:24:18 -0500

    Creando controlador @RestController y EndPoint para listar

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -0,0 +1,22 @@
+package com.bolsadeideas.springboot.backend.apirest.models.services;
+
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import com.bolsadeideas.springboot.backend.apirest.modles.dao.IClienteDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ClienteServiceImpl implements IClienteService {
+
+    @Autowired
+    private IClienteDao clienteDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Cliente> findAll() {
+        return (List<Cliente>) clienteDao.findAll();
+    }
+}
