201ca336cf317781b6f189cd0f898bab88ebac4d
Johan Castano <jach_1997@hotmail.com>
2020-01-07 17:02:29 -0500

    Probando API Rest de las regiones en Postman

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
-INSERT INTO clientes(apellido, create_at, email, nombre) VALUES ('Guzman', '2018-01-01', 'A@G.com', 'Andres');
-INSERT INTO clientes(apellido, create_at, email, nombre) VALUES ('Guzman1', '2018-01-01', 'A1@G.com', 'Andres1');
-INSERT INTO clientes(apellido, create_at, email, nombre) VALUES ('Guzman2', '2018-01-01', 'A2@G.com', 'Andres2');
+INSERT INTO regiones(id,nombre) VALUES (1,'Sudamerica');
+INSERT INTO clientes(region_id,apellido, create_at, email, nombre) VALUES (1,'Guzman', '2018-01-01', 'A@G.com', 'Andres');
+INSERT INTO clientes(region_id,apellido, create_at, email, nombre) VALUES (1,'Guzman1', '2018-01-01', 'A1@G.com', 'Andres1');
+INSERT INTO clientes(region_id,apellido, create_at, email, nombre) VALUES (1,'Guzman2', '2018-01-01', 'A2@G.com', 'Andres2');
