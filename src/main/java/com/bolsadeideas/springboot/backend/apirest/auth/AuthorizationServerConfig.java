ce5f1e2b2fc25d88a8a3fce96e3ae12a3bc12d62
Johan Castano <jach_1997@hotmail.com>
2020-01-08 11:11:59 -0500

    Añadiendo configuración de las aplicaciones clientes y acceso a endpoints

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/auth/AuthorizationServerConfig.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/auth/AuthorizationServerConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
@@ -23,25 +24,31 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        super.configure(security);
+        security.tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        super.configure(clients);
+        clients.inMemory().withClient("angularapp")
+                .secret(passwordEncoder.encode("12345"))
+                .scopes("read", "write")
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(3600)
+                .refreshTokenValiditySeconds(3600);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager)
-//                .tokenStore(tokenStore())
+                .tokenStore(tokenStore())
                 .accessTokenConverter(accessTokenConverter());
     }
 
-//    @Bean
-//    public JwtTokenStore tokenStore() {
-//        return new JwtTokenStore(accessTokenConverter());
-//    }
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
 
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
